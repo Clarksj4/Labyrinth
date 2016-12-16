d09373449b07c5840b6abec77952e0f570c11899
Clarksj4 <clarksj4@student.op.ac.nz>
2016-12-17 00:49:55 +1300

    Add AudioListener

---
+++ app/src/main/java/bit/clarksj4/labyrinth/Engine/AudioListener.java
@@ -0,0 +1,18 @@
+package bit.clarksj4.labyrinth.Engine;
+
+import android.media.MediaPlayer;
+import android.media.SoundPool;
+
+/**
+ * Created by StickasaurusRex on 16-Dec-16.
+ */
+
+public class AudioListener extends Component
+{
+
+
+    public AudioListener(GameObject gameObject)
+    {
+        super(gameObject);
+    }
+}
